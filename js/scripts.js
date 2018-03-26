f09f5206373ea09410eb97d5fd556fc49d759761
Marcin Różański <rozanskimarcin2016@gmail.com>
2018-03-26 12:16:28 +0200

    corrected js and index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,12 @@ function Phone(brand, price, color, camera) {
 }
 
 Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + ", price is " + this.price + "." and the camera is " + this.camera + ".");
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + ", price is " + this.price + " and the camera is " + this.camera + ".");
 }
 
-var samsungGalaxy6s = new Phone("Galaxy6s", 1500, "pink", 8Mp);
-var iPhone6S = new Phone("Apple", 2250, "silver", 10Mp);
-var onePlusOne = new Phone("PlusOne", 2500, "grey", 12Mp);
+var samsungGalaxy6s = new Phone("Galaxy6s", "1500", "pink", "8Mp");
+var iPhone6S = new Phone("Apple", "2250", "silver", "10Mp");
+var onePlusOne = new Phone("PlusOne", "2500", "grey", "12Mp");
 
 samsungGalaxy6s.printInfo();
 iPhone6S.printInfo();
